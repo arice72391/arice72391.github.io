23a6996643a5066b25324d0b47ee363761848a66
arice72391 <a.rice72391@gmail.com>
2019-07-21 20:41:38 -0400

    update 7.21

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "sales@swamprabbittech.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You! Someone will be in contact with you shortly.";
+?>
